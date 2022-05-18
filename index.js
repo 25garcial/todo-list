dbf1587f2a1b0f7bc6666575f8fd39290d00eb12
25garcial <25garcial@camraiders.com>
2022-05-18 09:48:06 -0500

    .

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var container = document.querySelector(".container")
 const projectButton = document.querySelector(".toDoMaker")
 projectButton.addEventListener("click", makeProject)
 let projectCount = 0
-const crayon = document.createElement("img");
-crayon.src = "crayon.png"
 let data = {}
 
 function Project() {
@@ -20,15 +18,28 @@ function makeAToDo(parent) {
   toDoRemoveButton.textContent = "-"
 
   toDoRemoveButton.classList.add("toDoRemoveButton")
-  toDoRemoveButton.id = projectCount-1
+  toDoRemoveButton.id = projectCount - 1
   toDo.classList.add("toDo")
-  toDo.textContent = "To Do: "
+  let text = document.createElement("A")
+  text.textContent = "To Do: "
 
-  toDoRemoveButton.addEventListener("click", function() {
+  text.addEventListener("click", function () { let input = document.createElement("input");
+   this.parentElement.append(input);
+    input.value = this.textContent;
+    this.textContent=""
+     let submit = document.createElement("input")
+     submit.type = "submit";
+     submit.classList.add("submit")
+   this.parentElement.append(submit);
+   submit.addEventListener("click", function(){text.textContent=input.value;input.remove(); this.remove()})
+     }
+    )
+  toDo.appendChild(text)
+  toDoRemoveButton.addEventListener("click", function () {
     this.parentElement.remove();
     data[`project${this.id}`].toDos -= 1;
-    if ( data[`project${this.id}`].toDos==0){
-    data[`project${this.id}`].htmlElement.remove()
+    if (data[`project${this.id}`].toDos == 0) {
+      data[`project${this.id}`].htmlElement.remove()
     }
   })
 
@@ -41,26 +52,42 @@ function createAddToDoButton() {
   const addToDoButton = document.createElement("button")
   addToDoButton.classList.add("addToDoButton")
   addToDoButton.textContent = "+"
-  addToDoButton.id=projectCount
-  addToDoButton.addEventListener("click", function() {
+  addToDoButton.id = projectCount
+  addToDoButton.addEventListener("click", function () {
     makeAToDo(this.parentElement);
 
     data[`project${this.id}`].toDos += 1;
     console.log(data);
   })
   return addToDoButton
-
 }
 
 function makeProject() {
   const projectBox = document.createElement("div")
   data[`project${projectCount}`] = new Project()
-  data[`project${projectCount}`].htmlElement=projectBox
+  data[`project${projectCount}`].htmlElement = projectBox
   let title = document.createElement("span")
+  title.classList.add("title")
+
+  title.addEventListener("click", function () { 
+    let input = document.createElement("input");
+   this.parentElement.append(input);
+    input.value = this.textContent;
+    this.textContent=""
+     let submit = document.createElement("input")
+     submit.type = "submit";
+     submit.classList.add("submit")
+     submit.classList.add("title")
+   this.parentElement.append(submit);
+   submit.addEventListener("click", function(){
+     title.textContent=input.value;
+     input.remove();
+    this.remove()})}
+    )
   projectBox.id = data[`project${projectCount}`].title
   title.textContent = projectBox.id
-  title.append(crayon)
-   projectBox.appendChild(title)
+
+  projectBox.appendChild(title)
 
   projectBox.classList.add("project")
   projectBox.appendChild(createAddToDoButton())
